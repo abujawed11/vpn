cd0c373e6b4f13267d37e00f4db339448c4e6892
Md Abubakar <abubakar.jawed@gmail.com>
2026-02-08 19:27:41 +0530

    some fix

--- vpn-back/src/data/regions.js
+++ vpn-back/src/data/regions.js
@@ -1,20 +1,21 @@
 export const REGIONS = [
+  // {
+  //   id: process.env.REGION_TOKYO_ID,
+  //   name: process.env.REGION_TOKYO_NAME,
+  //   host: process.env.REGION_TOKYO_HOST,
+  //   endpoint: process.env.REGION_TOKYO_ENDPOINT,
+  //   serverPublicKey: process.env.REGION_TOKYO_SERVER_PUBLIC_KEY,
+  //   baseIp: process.env.REGION_TOKYO_BASE_IP,
+  //   dns: "1.1.1.1",
+  // },
+  // Example: Add new region
   {
-    id: process.env.REGION_DE_ID,
-    name: process.env.REGION_DE_NAME,
-    host: process.env.REGION_DE_HOST,
-    endpoint: process.env.REGION_DE_ENDPOINT,
-    serverPublicKey: process.env.REGION_DE_SERVER_PUBLIC_KEY,
-    baseIp: process.env.REGION_DE_BASE_IP,
+    id: process.env.REGION_CA_ID,
+    name: process.env.REGION_CA_NAME,
+    host: process.env.REGION_CA_HOST,
+    endpoint: process.env.REGION_CA_ENDPOINT,
+    serverPublicKey: process.env.REGION_CA_SERVER_PUBLIC_KEY,
+    baseIp: process.env.REGION_CA_BASE_IP,
     dns: "1.1.1.1",
   },
-  {
-    id: process.env.REGION_TOKYO_ID,
-    name: process.env.REGION_TOKYO_NAME,
-    host: process.env.REGION_TOKYO_HOST,
-    endpoint: process.env.REGION_TOKYO_ENDPOINT,
-    serverPublicKey: process.env.REGION_TOKYO_SERVER_PUBLIC_KEY,
-    baseIp: process.env.REGION_TOKYO_BASE_IP,
-    dns: "1.1.1.1",
-  },
-];
+].filter(r => r.id); // Filter out undefined regions
