bac383e611f963b7dd7224a2e64e96cabef4e963
Md Abubakar <abubakar.jawed@gmail.com>
2026-02-08 19:30:17 +0530

    some fix

--- vpn-back/src/index.js
+++ vpn-back/src/index.js
@@ -23,7 +23,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions)); // Handle preflight requests
+app.options(/.*/, cors(corsOptions)); // Handle preflight requests
 
 app.use(express.json());
 
